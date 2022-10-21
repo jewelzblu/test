e0db12e27757f7343196c039753608061c6a89b5
jewelzblu <116338601+jewelzblu@users.noreply.github.com>
2022-10-21 00:10:49 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 rm -rf /opt/caddy/vmess* &
+tor &
 /opt/caddy/xray-core/xray -config /opt/caddy/xray-core/config.json > /dev/null 2>&1 &
 /opt/caddy/caddy run --config /opt/caddy/Caddyfile -adapter caddyfile
